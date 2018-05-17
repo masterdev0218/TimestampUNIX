2998741efb6c8999d6a9c2b486f5e98a14cc865e
Glitch (timestamp-microservice-ajb) <none>
2018-05-17 15:11:13 +0000

    👧😨 Checkpoint ./server.js:1080052/56

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ var app = module.exports = express();
 app.use(bodyParser.json());
 app.use(cors());
 
+app.get('date', function(req, res, next){
+  
+});
+
 // listen for requests
 app.listen(3000, function () {
   console.log('Your app is listening');
