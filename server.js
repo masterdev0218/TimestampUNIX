48577865cf4b22f95889e56a95a7cb83b845561d
Glitch (timestamp-microservice-ajb) <none>
2018-05-18 16:47:31 +0000

    🍠🖼 Checkpoint ./server.js:1080052/452

--- server.js
+++ server.js
@@ -11,12 +11,23 @@ app.use(cors());
 
 //GET call for data parameters
 app.get('/date/:dateValue', function(req,res,next){
+  //gets unix code user input to be formatted
   var dateValue = req.params.dateValue;
   
-  var date
+  //Date formatting options
+  var optionsDateFormatting = {
+      year: 'numeric',
+      month: 'long',
+    day: 'numeric'
+  };
   
-  res.json({unix: dateValue});
+  if(isNaN(dateValue)){
+    var naturalDate = new Date(dateValue);
+    naturalDate = naturalDate.toLocaleDateString("en-us", optionsDateFormatting);
+    var unixDAte = new Date(dateValue).getTime()/1000;
+  }
   
+  res.json({unix: dateValue, natural: naturalDate}); 
 });
 
 // listen for requests
