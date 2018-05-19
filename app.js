139f4d7334dbc27c71cc0db84c0a8fb97de5d7ee
Glitch (timestamp-microservice-ajb) <none>
2018-05-19 09:33:09 +0000

    💅💀 Checkpoint ./app.js:1080052/43

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use(bodyParser.json());
 app.use(cors());
 
 //GET call for data parameters
-app.get('/date/:dateValue', function(req,res,next){
+app.get('/', function(req,res,next){
   //gets unix code user input to be formatted
   var dateValue = req.params.dateValue;
   
