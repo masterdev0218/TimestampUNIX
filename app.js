c4762b9d5619cca2d287e1ec674bb9829ef412d3
Glitch (timestamp-microservice-ajb) <none>
2018-05-19 10:05:09 +0000

    🙅🌏 Checkpoint ./app.js:1080052/446

--- app.js
+++ app.js
@@ -3,16 +3,32 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var cors = require('cors');
+var path = require('path');
 
 //create instance of express. Instantiate bodyParser and cors
 var app = module.exports = express();
 app.use(bodyParser.json());
 app.use(cors());
 
+//GET call
+app.get('/', function(req,res){
+  var fileName = path.join(__dirname, 'index.html');
+  res.sendFile(fileName, function(err) {
+    if (err){
+     console.log(err);
+      res.status(err.status).end();
+    }
+    else {
+     console.log('Sent:', fileName); 
+    }
+  });
+});
+
+
 //GET call for data parameters
 app.get('/', function(req,res,next){
   //gets unix code user input to be formatted
-  var dateValue = req.params.dateValue;
+  var dateValue = req.params.datestring;
   
   //Date formatting options
   var format = {
