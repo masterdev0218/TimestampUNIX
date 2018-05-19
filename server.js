5eed052e67b496b4e014f72b280cbfeb41aa4606
Glitch (timestamp-microservice-ajb) <none>
2018-05-19 13:52:17 +0000

    😰⏲ Checkpoint ./package.json:1080052/50 ./server.js:1080052/179

--- server.js
+++ server.js
@@ -1,17 +1,19 @@
 
 // init project
-var express = require('express'); //a minimal and flexible node.js web application framework
-var bodyParser = require('body-parser'); //Parse incoming request bodies in a middleware before your handlers
+const cool = require('cool-ascii-faces'); //added for Heroku
+vconst express = require('express'); //a minimal and flexible node.js web application framework
+co bodyParser = require('body-parser'); //Parse incoming request bodies in a middleware before your handlers
 var cors = require('cors'); //Cross Origin Resource Sharing, allows use of REST API served from a different origin
 var path = require('path'); //utilities for working with file and directory paths
 var fs = require('fs'); //API for interacting with the file system
+const port = process.env.PORT || 8080; //set port
 
 //create instance of express. Instantiate bodyParser and cors
 var app = module.exports = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-var port = process.env.PORT || 8080; //set port
+
 // listen for requests
 app.listen(port, () => {
   console.log('Your app is listening on port ' + port);
